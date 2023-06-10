77608922e81255c282bd189f454f6970271766cb
Jaron van Well <jaronvwell@gmail.com>
2023-06-10 11:52:40 +0200

    api ffmpeg stream test

--- src/Chat/chat.gateway.ts
+++ src/Chat/chat.gateway.ts
@@ -9,7 +9,7 @@ import { ChatService } from './chat.service';
 import { Server, Socket } from 'socket.io';
 import { AuthUser } from 'src/auth.decorator';
 import { JWTDecodedUser } from 'src/jwt.decoded.user';
-
+import * as child from 'child_process';
 @WebSocketGateway({ cors: true })
 export class ChatGateway {
   constructor(private service: ChatService) {}
@@ -29,5 +29,30 @@ export class ChatGateway {
         streamId: streamId,
       });
     });
+  }
+
+  @SubscribeMessage('data')
+  async getData(@MessageBody() data: string) {
+    const ffmpeg = child.spawn('ffmpeg', [
+      '-f',
+      'lavfi',
+      '-i',
+      'anullsrc',
+      '-i',
+      '-',
+      '-c:v',
+      'libx264',
+      '-preset',
+      'veryfast',
+      '-tune',
+      'zerolatency',
+      '-c:a',
+      'aac',
+      '-f',
+      'flv',
+      `rtmp://127.0.0.1:1935/live/key`,
+    ]);
+    console.log(data);
+    ffmpeg.stdin.write(data);
   }
 }
