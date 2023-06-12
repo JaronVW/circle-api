799c9bff9d77fb3ae074452f70d051568474bae3
Jaron van Well <jaronvwell@gmail.com>
2023-06-12 17:15:43 +0200

    websocket auth wip

--- src/Chat/chat.gateway.ts
+++ src/Chat/chat.gateway.ts
@@ -10,37 +10,36 @@ import { Server, Socket } from 'socket.io';
 import { AuthUser } from 'src/auth.decorator';
 import { JWTDecodedUser } from 'src/jwt.decoded.user';
 import * as child from 'child_process';
+import { UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+import { MessageDto } from './message.dto';
+
 @WebSocketGateway({ cors: true })
 export class ChatGateway {
   constructor(private service: ChatService) {}
 
+  @WebSocketServer()
+  server: Server;
+
+  @UseGuards()
   @SubscribeMessage('chat')
   async joinStream(
     @MessageBody() streamId: string,
     @ConnectedSocket() socket: Socket,
     @AuthUser() user: JWTDecodedUser,
   ) {
+    // console.log('joinStream', streamId);
     socket.join(streamId);
-    socket.on(streamId, (message) => {
-      socket.to(streamId).emit(streamId, message);
-      this.service.PostMessage({
-        message: message,
-        userId: user.userId,
-        streamId: streamId,
-      });
+    socket.on(streamId, (message: MessageDto) => {
+      console.log('message', message);
+      message.datetime = new Date();
+      this.server.to(streamId).emit(streamId, message);
+      // return message;
+      // this.service.PostMessage({
+      //   message: message,
+      //   userId: user.userId,
+      //   streamId: streamId,
+      // });
     });
-  }
-
-  @SubscribeMessage('data')
-  async getData(@MessageBody() data: string) {
-    const ffmpeg = child.spawn('ffmpeg', [
-      'ffmpeg -re -i _ -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/STREAM_NAME',
-    ]);
-
-    ffmpeg.stderr.on('data', (data) => {
-      console.log('FFmpeg STDERR:', data.toString());
-    });
-    console.log(data);
-    ffmpeg.stdin.write(data);
   }
 }
