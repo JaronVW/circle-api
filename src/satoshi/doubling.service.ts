4e2989bfd2388743012a9d5f9ebf45988aa9fe12
sanneve <s.vaneck2@student.avans.nl>
2023-06-15 14:53:21 +0200

    logging of chats messages

--- src/satoshi/doubling.service.ts
+++ src/satoshi/doubling.service.ts
@@ -4,11 +4,9 @@ import { Injectable } from '@nestjs/common';
 export class DoublingService {
   doubleNumberNTimes(number: number, times: number): number {
     let result = number;
-
     for (let i = 0; i < times; i++) {
       result *= 2;
     }
-
     return result;
   }
 }
